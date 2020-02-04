4769461c2828407397e72831daf0d8070f8297e7
Josh Simmons <001simmoj@pcsmac001009.local>
2020-02-03 16:57:44 -0800

    modal up

--- misc/slick-employee.js
+++ misc/slick-employee.js
@@ -3,7 +3,7 @@ const injectAndSlick = () => {
 
     // Links to all the image - smashes to HTML. replace the data-img attr with the link to the modal image
     var images = ['https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg'].map((imageLink) => {
-        return `<img class='img-responsive' alt='Employee photos' src='${imageLink}' data-img='${imageLink}' role='button' onclick="window.modalSpawn(this)"/>`
+        return `<img class='img-responsive' alt='Employee photos' src='${imageLink}' data-img='${imageLink}' data-toggle="modal" data-target="#myModal" role='button' onclick="window.modalSpawn(this)"/>`
     }).join("")
 
     injectionPoint.empty()
