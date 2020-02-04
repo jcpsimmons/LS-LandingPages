49946b94e4f8dd681aa47af0ba0db1337386a042
Josh Simmons <001simmoj@pcsmac001009.local>
2020-02-03 16:30:20 -0800

    x

--- Scripts/workFrontElim.js
+++ Scripts/workFrontElim.js
@@ -1,7 +1,34 @@
 // Workfront Finisher
-setInterval(() => {
-  document.querySelector("button.DoneButton").click();
-  document
-    .querySelector("li.pick-list-item.pickable.pos-r.ellipsed-text")
-    .click();
-}, 250);
+
+// Navigate to https://livingspaces.my.workfront.com/myWork and run:
+
+// Go to requests
+document.querySelector('[data-name=list-myWork-workRequest]').click()
+
+
+const finish = () => {
+  document.querySelector('[data-name=list-myWork-workingOn]').click()
+  // FINISH IT - use on 'Working On' tab
+  let finishIt = setInterval(() => {
+
+    document.querySelector("button.DoneButton").click();
+    document
+      .querySelector("li.pick-list-item.pickable.pos-r.ellipsed-text")
+      .click();
+
+
+  }, 500);
+}
+
+// Click requests (add them to your working on queue)
+let workOnIt = setInterval(() => {
+  if (document.querySelectorAll(".Button.work-on-it.primary.btn.btn-primary").length) {
+    document
+      .querySelector(".Button.work-on-it.primary.btn.btn-primary")
+      .click();
+  }
+  else {
+    finish()
+    window.clearInterval(workOnIt)
+  }
+}, 500);
---
+++ misc/slick-employee.js
@@ -0,0 +1,32 @@
+const injectAndSlick = () => {
+    var injectionPoint = $('#EmployeePhotoSlider')
+
+    // Links to all the image - smashes to HTML. replace the data-img attr with the link to the modal image
+    var images = ['https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg', 'https://via.placeholder.com/200.jpg'].map((imageLink) => {
+        return `<img class='img-responsive' alt='Employee photos' src='${imageLink}' data-img='${imageLink}' role='button' onclick="window.modalSpawn(this)"/>`
+    }).join("")
+
+    injectionPoint.empty()
+    injectionPoint.append(images)
+    injectionPoint.slick({
+        infinite: true,
+        slidesToShow: 6,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000
+    })
+
+    // Gets the data-img attr and makes a modal
+    window.modalSpawn = (e) => {
+        console.log(e.dataset.img)
+    }
+
+}
+
+
+const jqwait = setInterval(() => {
+    if (window.jQuery) {
+        injectAndSlick()
+        window.clearInterval(jqwait)
+    }
+}, 50)
