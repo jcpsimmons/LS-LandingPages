73dca6b5a4e0b3f1b0eab6e3b50a374eaf59e47f
Josh Simmons <001simmoj@pcsmac001009.local>
2020-02-05 10:06:26 -0800

    transpile boomerscript vers

--- misc/slick-employee-boomerscript.js
+++ misc/slick-employee-boomerscript.js
@@ -1,66 +1,85 @@
 "use strict";
 
 var injectAndSlick = function injectAndSlick() {
-    var injectionPoint = $("#EmployeePhotoSlider"); // Configure Brandfolder Links
+  var injectionPoint = $("#EmployeePhotoSlider"); // Configure Brandfolder Links
 
-    var imageLinks = [
-        "https://cdn.brandfolder.io/XQW73PF4/as/q56vv4-bkjfnc-3nfzka/Team_LS_Photos_for_Webpage.jpg?width=200&height=200"
-    ];
+  var imageLinks = [
+    "https://cdn.brandfolder.io/XQW73PF4/as/q56vv4-bkjfnc-3nfzka/Team_LS_Photos_for_Webpage.jpg?width=200&height=200"
+  ];
 
-    for (var i = 2; i < 12; i++) {
-        imageLinks.push("".concat(imageLinks[0], "&position=").concat(i));
-    } // Links to all the image - smashes to HTML. replace the data-img attr with the link to the modal image
+  for (var i = 2; i < 12; i++) {
+    imageLinks.push("".concat(imageLinks[0], "&position=").concat(i));
+  } // Links to all the image - smashes to HTML. replace the data-img attr with the link to the modal image
 
-    var images = imageLinks
-        .map(function (imageLink) {
-            return "<img class='img-responsive' alt='Employee photos' src='"
-                .concat(imageLink, "' data-img='")
-                .concat(
-                    imageLink.replace(/200/gi, "500"),
-                    '\' data-toggle="modal" data-target="#PhotoModal" role=\'button\' onclick="window.modalSpawn(this)"/>'
-                );
-        })
-        .join("");
-    injectionPoint.empty();
-    injectionPoint.append(images);
+  var images = imageLinks
+    .map(function(imageLink) {
+      return "<img class='img-responsive' alt='Employee photos' src='"
+        .concat(imageLink, "' data-img='")
+        .concat(
+          imageLink.replace(/200/gi, "500"),
+          '\' data-toggle="modal" data-target="#PhotoModal" role=\'button\' onclick="window.modalSpawn(this)"/>'
+        );
+    })
+    .join("");
+  injectionPoint.empty();
+  injectionPoint.append(images);
+
+  if (utag_data.site_type == "mobile") {
     injectionPoint.slick({
-        infinite: true,
-        slidesToShow: 6,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 4000
-    }); // Triggered by photo click
-    // Gets the data-img attr and makes a modal
-
-    window.modalSpawn = function (e) {
-        document.querySelector("#PhotoModal img").src = e.dataset.img;
-    }; // Triggered by x click on modal
-
-    window.closeModal = function () {
-        $("#PhotoModal").modal("hide");
-    }; // ADA Compliance - trigger modal with keypress
-
-    $("#EmployeePhotoSlider img").keydown(function (e) {
-        if (e.keyCode === 13) $(this).trigger("click");
-    }); // Close modal with ESC key
-
-    $(document).on("keydown", function (event) {
-        if (event.key == "Escape") {
-            $("#PhotoModal").modal("hide");
-        }
-
-        if (
-            event.key == "Enter" &&
-            document.querySelector("#PhotoModal i.fa-times:focus")
-        ) {
-            window.closeModal();
-        }
+      infinite: true,
+      slidesToShow: 3,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 4000
     });
+  } else {
+    injectionPoint.slick({
+      infinite: true,
+      slidesToShow: 6,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 4000
+    });
+  } // Triggered by photo click
+  // Gets the data-img attr and makes a modal
+
+  window.modalSpawn = function(e) {
+    document.querySelector("#PhotoModal img").src = e.dataset.img;
+  }; // Triggered by x click on modal
+
+  window.closeModal = function() {
+    $("#PhotoModal").modal("hide");
+  }; // ADA Compliance - trigger modal with keypress
+
+  $("#EmployeePhotoSlider img").keydown(function(e) {
+    if (e.keyCode === 13) $(this).trigger("click");
+  }); // Close modal with ESC key
+
+  $(document).on("keydown", function(event) {
+    if (event.key == "Escape") {
+      $("#PhotoModal").modal("hide");
+    }
+
+    if (
+      event.key == "Enter" &&
+      document.querySelector("#PhotoModal i.fa-times:focus")
+    ) {
+      window.closeModal();
+    }
+  }); // Focus trap events
+
+  $("#PhotoModal").on("shown.bs.modal", function() {
+    $(document).on("keydown", function(event) {
+      if (event.key == "Tab") {
+        $("#PhotoModal").focus();
+      }
+    });
+  });
 };
 
-var jqwait = setInterval(function () {
-    if (window.jQuery) {
-        injectAndSlick();
-        window.clearInterval(jqwait);
-    }
+var jqwait = setInterval(function() {
+  if (window.jQuery) {
+    injectAndSlick();
+    window.clearInterval(jqwait);
+  }
 }, 50);
